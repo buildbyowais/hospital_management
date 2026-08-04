93877d17ae193cb480a34096088709c2f801d62e
owais-js <owaisshabbir3131@gmail.com>
2026-08-04 22:34:53 +0500

    user router added in main.py

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from fastapi import FastAPI
 from app.api.patient import router as patient_router
 from app.api.doctor import router as doctor_router
 from app.api.staff import router as staff_router
+from app.api.auth import router as auth_router
 
 app = FastAPI(
     title="Hospital Management API"
@@ -11,6 +12,7 @@ app = FastAPI(
 app.include_router(patient_router)
 app.include_router(doctor_router)
 app.include_router(staff_router)
+app.include_router(auth_router)
 
 @app.get("/")
 def home():
