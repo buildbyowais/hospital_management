77f0904a67208fe0d987c08c15187ebfc8afc0b9
owais-js <owaisshabbir3131@gmail.com>
2026-08-05 08:55:19 +0500

    role check functionality added

--- app/api/auth.py
+++ app/api/auth.py
@@ -117,3 +117,16 @@ def refresh_token_route(refresh_token: str, db: Session = Depends(get_db)):
         "refresh_token": new_refresh_token,
         "token_type": "bearer"
     }
+
+def require_role(allowed_roles : list[str]):
+    def role_checker(
+            current_user : User = Depends(get_current_user)
+    ):
+        if current_user.role not in allowed_roles:
+            raise HTTPException(
+                status_code=403,
+                detail="Not Enough Permissions!"
+            )
+        return current_user
+    return role_checker
+    
