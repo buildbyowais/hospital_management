bfdaae1389b45d53a6ff2461e96ca7c4713ec798
owais-js <owaisshabbir3131@gmail.com>
2026-08-06 08:15:20 +0500

    add relationship of doctor in user model

--- app/models/user.py
+++ app/models/user.py
@@ -1,4 +1,5 @@
 from sqlalchemy import Column, Integer, String
+from sqlalchemy.orm import relationship
 from app.core.database import Base
 
 
@@ -10,3 +11,5 @@ class User(Base):
     email = Column(String(100), unique=True, index=True, nullable=False)
     hashed_password = Column(String(255), nullable=False)
     role = Column(String(50),nullable=False)
+
+    doctor = relationship("Doctor", back_populates="user", uselist=False)
