5492fd6ccc6f2e5a45a663e3e754c3c16d7305da
owais-js <owaisshabbir3131@gmail.com>
2026-08-06 19:00:27 +0500

    add user_id and email also logic user-patient deletion

--- app/models/patient.py
+++ app/models/patient.py
@@ -11,8 +11,22 @@ class Patient(Base):
     age = Column(Integer, nullable=False)
     gender = Column(String(20), nullable=False)
     phone = Column(String(20), nullable=False)
+    email = Column(String(100), unique=True, nullable=False, index=True)
 
+    #Foreign key linked patient with user table
+    user_id = Column(Integer, ForeignKey("users.id"), nullable=True, unique=True)
+    user= relationship("User",back_populates="patient")
+    
     # Foreign Key with ON DELETE CASCADE
     doctor_id = Column(Integer, ForeignKey("doctors.id", ondelete="CASCADE"), nullable=False)
-
     doctor = relationship("Doctor", back_populates="patients")
+
+
+    # Auto-deletes linked user account on patient deletion
+    user = relationship(
+        "User", 
+        back_populates="patient", 
+        uselist=False, 
+        cascade="all, delete-orphan", 
+        single_parent=True
+    )
