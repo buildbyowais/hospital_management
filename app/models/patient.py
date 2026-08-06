24c6e9dd09da9c0beacaf76eeeb075b00c9044ca
owais-js <owaisshabbir3131@gmail.com>
2026-08-06 08:16:26 +0500

    add relationship of doctor in patient model for accessing desired patients

--- app/models/patient.py
+++ app/models/patient.py
@@ -1,5 +1,5 @@
 from sqlalchemy import Column, Integer, String,ForeignKey
-
+from sqlalchemy.orm import relationship
 from app.core.database import Base
 
 
@@ -11,4 +11,8 @@ class Patient(Base):
     age = Column(Integer, nullable=False)
     gender = Column(String(20), nullable=False)
     phone = Column(String(20), nullable=False)
-    doctor_id = Column(Integer, ForeignKey("doctors.id"), nullable=False)
+
+    # Foreign Key with ON DELETE CASCADE
+    doctor_id = Column(Integer, ForeignKey("doctors.id", ondelete="CASCADE"), nullable=False)
+
+    doctor = relationship("Doctor", back_populates="patients")
