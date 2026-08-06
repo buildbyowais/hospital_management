8a736b58e0c408ad0ebcbf1ad51e38c4030d3ed3
owais-js <owaisshabbir3131@gmail.com>
2026-08-06 10:15:09 +0500

    updated staff add and update functions

--- app/crud/staff.py
+++ app/crud/staff.py
@@ -7,10 +7,12 @@ def create_staff(db:Session,staff:StaffCreate):
     db_staff = Staff(
         name = staff.name,
         gender = staff.gender,
+        email = staff.email,
         designation = staff.designation,
         department = staff.department,
         phone = staff.phone,
-        salary = staff.salary
+        salary = staff.salary,
+        user_id = None
     )
     db.add(db_staff)
     db.commit()
@@ -39,6 +41,7 @@ def update_staff(
         return None
     db_staff.name = staff.name
     db_staff.gender = staff.gender
+    db_staff.email = staff.email,
     db_staff.designation = staff.designation
     db_staff.department = staff.department
     db_staff.phone = staff.phone
@@ -61,4 +64,5 @@ def delete_staff(
     db.commit()
 
     return db_staff
+
 
