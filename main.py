7ee63aae17174769b5018bac0dc848bd40407e8d
owais-js <owaisshabbir3131@gmail.com>
2026-08-07 17:14:53 +0500

    prescription router added in main.py

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ from app.api.doctor import router as doctor_router
 from app.api.staff import router as staff_router
 from app.api.auth import router as auth_router
 from app.api.appointment import router as appointment_router
+from app.api.prescription import router as prescription_router
 
 app = FastAPI(
     title="Hospital Management API"
@@ -15,6 +16,7 @@ app.include_router(doctor_router)
 app.include_router(staff_router)
 app.include_router(auth_router)
 app.include_router(appointment_router)
+app.include_router(prescription_router)
 
 @app.get("/")
 def home():
