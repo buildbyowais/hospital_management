b3db39336c7e300a0471e64dcd2eb4c940e198a6
owais-js <owaisshabbir3131@gmail.com>
2026-08-07 23:28:05 +0500

    add patient-model router

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ from app.api.staff import router as staff_router
 from app.api.auth import router as auth_router
 from app.api.appointment import router as appointment_router
 from app.api.prescription import router as prescription_router
+from app.api.report import router as patient_report_router
 
 app = FastAPI(
     title="Hospital Management API"
@@ -17,6 +18,7 @@ app.include_router(staff_router)
 app.include_router(auth_router)
 app.include_router(appointment_router)
 app.include_router(prescription_router)
+app.include_router(patient_report_router)
 
 @app.get("/")
 def home():
