923db1e96e7865d042a8a6884a5fad94bb1eef33
owais-js <owaisshabbir3131@gmail.com>
2026-08-18 21:05:55 +0500

    limit updated

--- app/api/patient.py
+++ app/api/patient.py
@@ -208,7 +208,7 @@ def read_all(
     doctor_id: int | None = None,
     skip: int = 0,
     limit: int = Query(
-        10,
+        1000,
         ge=1,
         le=100
     ),
--- app/api/staff.py
+++ app/api/staff.py
@@ -125,7 +125,7 @@ def read_all(
     search: str | None =None,
     designation : str | None = None,
     skip:int = 0,
-    limit:int = Query(10,ge=1,le=100),
+    limit:int = Query(10,ge=1,le=500),
     db:Session = Depends(get_db),
     current_user : User = Depends(
         require_role(["admin"])
